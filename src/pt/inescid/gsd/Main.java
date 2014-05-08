f7bec5f8d3d9cdf640f06f229ca11876c55470a9
afatc <alexandrefilipecampos@gmail.com>
2014-05-08 18:23:42 +0100

    first test

--- src/pt/inescid/gsd/Main.java
+++ src/pt/inescid/gsd/Main.java
@@ -1,36 +1,59 @@
+package pt.inescid.gsd;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.ResultScanner;
+import org.apache.hadoop.hbase.client.Scan;
+
+import com.yahoo.omid.transaction.*;
 
 
 
 
 public class Main {
 
+	protected final static String TEST_TABLE = "test";
+	protected final static String ROW = "row1";
+	protected final static String FAM = "cf1";
+	protected final static String COL = "cl1";
+	protected final static String data = "The restaurant at the end of the universe";
 
+  private static void executeTransaction () throws Exception {
+		Configuration conf = HBaseConfiguration.create();
+		TransactionManager tm = new TransactionManager(conf);
+		TTable tt = new TTable(conf, TEST_TABLE);
 
-
-  private static void execute-transaction () {
-      Configuration conf = HBaseConfiguration.create();
-      TransactionManager tm = new TransactionManager(conf);
-      TransactionalTable tt = new TransactionalTable(conf, TEST_TABLE);
-
-      TransactionState t1 = tm.beginTransaction();
-
-      Put put = new Put(row);
-      putt.add(fam, col, data);
-      tt.put(t1, p);
-
-      ResultScanner rs = tt.getScanner(t1, new Scan().setStartRow(startrow).setStopRow(stoprow));
-      Result r = rs.next();
-      while (r != null) {
-         ...
-         r = rs.next();
-      }
-      tm.tryCommit(t1);
+    Transaction t1 = tm.begin();
+     
+    Put put = new Put(ROW.getBytes());
+    put.add(FAM.getBytes(), COL.getBytes(), data.getBytes());
+    
+    tt.put(t1, put);
+    
+    ResultScanner rs = 
+    		tt.getScanner(
+    				t1, 
+    				new Scan().setStartRow(ROW.getBytes()).setStopRow(ROW.getBytes()));
+    Result r = rs.next();
+    while (r != null) {
+    	
+    	r = rs.next();
+    }
+    
+    tm.commit(t1);
+    
    }
 
   
   public static void main(String[] args) {
 
-
+  	try{
+  		executeTransaction();
+  	} catch(Exception e){
+  		e.printStackTrace();
+  	}
   }
 
 }
